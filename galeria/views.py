04f9eb5d7b2013be18123b49172c8cbb9090c7b4
Jaimerson Santos <jaimerson.santos@tre-am.jus.br>
2026-04-08 11:15:11 -0400

    Templates

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Bem vindo ao Alura Space</p>')
+    return render(request, 'index.html')
 # Create your views here.
