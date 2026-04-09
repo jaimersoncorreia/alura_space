a4fac92aab079eeaf51962927df9668ab5927e55
Jaimerson Santos <jaimerson.santos@tre-am.jus.br>
2026-04-09 16:22:58 -0400

    Funcionalidade de publicação

--- galeria/models.py
+++ galeria/models.py
@@ -12,6 +12,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
